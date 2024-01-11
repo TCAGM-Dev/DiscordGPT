5c55cf82a98ddaddccdb8b6555ed094febfc81c4
TCAGM-Dev <thomas@andringa.org>
2024-01-11 18:12:07 +0100

    fix

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
-const data = require("database.json")
+const data = require("./database.json")
 
 process.on("exit", () => {
     fs.writeFileSync("database.json", JSON.stringify(data))
