a2321b0b96deb98e2e41bb6b262e436276742922
TCAGM-Dev <thomas@andringa.org>
2024-01-11 18:02:11 +0100

    file database

--- db.js
+++ db.js
@@ -1,2 +1,14 @@
-const Database = require("@replit/database")
-module.exports = new Database()
+const fs = require("fs")
+
+const data = require("database.json")
+
+process.on("exit", () => {
+    fs.writeFileSync("database.json", JSON.stringify(data))
+})
+
+module.exports = {
+    get: async key => data[key],
+    set: async (key, value) => data[key] = value,
+    delete: async key => delete data[key],
+    list: async prefix => Object.keys(data).filter(v => v.startsWith(prefix))
+}
